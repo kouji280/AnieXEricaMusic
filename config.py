df2a26755e76c000d1f50dcd83ed2e91a719a3a8
AYAME <167174581+kouji280@users.noreply.github.com>
2024-04-24 02:31:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,25 +6,25 @@ from pyrogram import filters
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID","6435225"))
-API_HASH = getenv("API_HASH","4e984ea35f854762dcde906dce426c2d")
+API_ID = int(getenv("API_ID","23167322"))
+API_HASH = getenv("API_HASH","9a684493404fc96d3ab58bd42a6d15eb")
 BOT_TOKEN = getenv("BOT_TOKEN","")
 MONGO_DB_URI = getenv("MONGO_DB_URI", "")
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", "9999"))
-LOGGER_ID = int(getenv("LOGGER_ID", "-1001971806089"))
-BOTADDLOGS = int(getenv("BOTADDLOGS", "-1001971806089")) # LOGGER_ID Id Also Use No Problem
-GBAN_LOGS = int(getenv("GBAN_LOGS", "-1002046393302"))
-GCAST_USERS = list(map(int, getenv("GCAST_USERS", "2105971379 5916859256").split()))
-OWNER_ID = int(getenv("OWNER_ID", 2105971379))
-OWNER = int(getenv("OWNER", 5916859256))
-OWNER_USERNAME = getenv("OWNER_USERNAME","sultan11100")
+LOGGER_ID = int(getenv("LOGGER_ID", "-1002044251414"))
+BOTADDLOGS = int(getenv("BOTADDLOGS", "-1002044251414")) # LOGGER_ID Id Also Use No Problem
+GBAN_LOGS = int(getenv("GBAN_LOGS", "-1002044251414"))
+GCAST_USERS = list(map(int, getenv("GCAST_USERS", "5817124748 6473663036").split()))
+OWNER_ID = int(getenv("OWNER_ID", 5322882174))
+OWNER = int(getenv("OWNER", 5322882174))
+OWNER_USERNAME = getenv("OWNER_USERNAME","koji280")
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
-UPSTREAM_REPO = getenv("UPSTREAM_REPO","https://github.com/AbhiModszYT/AnieXEricaMusic")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO","https://github.com/kouji280/AnieXEricaMusic")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv("GIT_TOKEN",None)
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Logs_Gban")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/AM_YTSUPPORT")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/pixel_support92")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/MONARCH_CHAT_AF")
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", True))
 AUTO_LEAVE_ASSISTANT_TIME = int(getenv("ASSISTANT_LEAVE_TIME", "5400"))
 SONG_DOWNLOAD_DURATION = int(getenv("SONG_DOWNLOAD_DURATION", 7000))
@@ -49,25 +49,25 @@ votemode = {}
 autoclean = []
 confirmer = {}
 AMBOT = [
-    "🔎",
-    "🔍",
+    "🦋",
+    "💐",
     "🧪",
     "ᴘʟꜱ ᴡᴀɪᴛ..",
     "ᴘʀᴏᴄᴇꜱꜱɪɴɢ..",
 ]
 
-START_IMG_URL = getenv("START_IMG_URL", "https://graph.org/file/f234fa4e140eb1b85d185.jpg")
-PING_IMG_URL = getenv("PING_IMG_URL", "https://graph.org/file/f234fa4e140eb1b85d185.jpg")
-PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
-STATS_IMG_URL = getenv("STATS_IMG_URL", "https://graph.org/file/f234fa4e140eb1b85d185.jpg")
-TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
-TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
-STREAM_IMG_URL = "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"
-SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/bb0ff85f2dd44070ea519.jpg"
-YOUTUBE_IMG_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/37d163a2f75e0d3b403d6.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://te.legra.ph/file/b35fd1dfca73b950b1b05.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://te.legra.ph/file/95b3ca7993bbfaf993dcb.jpg"
+START_IMG_URL = getenv("START_IMG_URL", "https://graph.org/file/463d4310c627d380cafa0.jpg")
+PING_IMG_URL = getenv("PING_IMG_URL", "https://graph.org/file/ce39c4084fc9ee10e585f.jpg")
+PLAYLIST_IMG_URL = "https://graph.org/file/2a6ada5925275c9942587.jpg"
+STATS_IMG_URL = getenv("STATS_IMG_URL", "https://graph.org/file/227f007a732d6e5abcf63.jpg")
+TELEGRAM_AUDIO_URL = "https://graph.org/file/1da5107b788b2d0cad8c8.jpg"
+TELEGRAM_VIDEO_URL = "https://graph.org/file/c6af6e71543e8a36ec2af.jpg"
+STREAM_IMG_URL = "https://graph.org/file/692d344953a9201b61e8f.jpg"
+SOUNCLOUD_IMG_URL = "https://graph.org/file/463d4310c627d380cafa0.jpg"
+YOUTUBE_IMG_URL = "https://graph.org/file/463d4310c627d380cafa0.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://graph.org/file/463d4310c627d380cafa0.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://graph.org/file/463d4310c627d380cafa0.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://graph.org/file/463d4310c627d380cafa0.jpg"
 
 
 def time_to_seconds(time):
